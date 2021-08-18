dc98ed92ac9b4b99b4a81f8c544a35840ada3b7d
Azka <a.azkanorouzi@gmail.com>
2021-08-18 16:14:09 +0430

    completed

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
-let myLeads = [];
+let myLeads = ["www.awesomelead.com"];
+myLeads = JSON.stringify(myLeads)
+myLeads.push("www.sdffsdf.com")
+console.log(myLeads);
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
-console.log(ulEl)
-inputBtn.addEventListener("click",function push(){
-myLeads.push(inputEl.value);
- for (let i = 0; i < myLeads.length; i++) {
-  ulEl.textContent += myLeads[i];
- }
+
+inputBtn.addEventListener("click", function () {
+
+ myLeads.push(inputEl.value);
+ renderLeads();
+ inputEl.value = "";
 })
+function renderLeads() {
+ let listItem = ""
+ for (let i = 0; i < myLeads.length; i++) {
+  listItem += `<li id='li-el'>
+<a target ='_blank' href='${myLeads[i]}'>
+  ${myLeads[i]}
+</a>
+</li>`;
+ }
+ ulEl.innerHTML = listItem
+}
+
+
 
 
 
