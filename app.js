79678472c8e3bd0378e19c5e1c18b06e29a7e36e
Azka <a.azkanorouzi@gmail.com>
2021-08-19 13:52:04 +0430

    completed

--- app.js
+++ app.js
@@ -1,29 +1,58 @@
-let myLeads = ["www.awesomelead.com"];
-myLeads = JSON.stringify(myLeads)
-myLeads.push("www.sdffsdf.com")
-console.log(myLeads);
+let myLeads =[];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+const tabs = [
+  {
+    url: "https://github.com/Azkanorouzi"
+  }
+]
+const leadsFromLocalStorage=JSON.parse(localStorage.getItem("myLeads"));
+const tabBtn = document.getElementById("tab-btn");
 
-inputBtn.addEventListener("click", function () {
+tabBtn.addEventListener("click" ,function(){
 
- myLeads.push(inputEl.value);
- renderLeads();
- inputEl.value = "";
+/*   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  }); */
+  chrome.tabs.query( {active: true, currentWindow: true}, function(tabs){
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  })
+
+
 })
-function renderLeads() {
- let listItem = ""
- for (let i = 0; i < myLeads.length; i++) {
-  listItem += `<li id='li-el'>
-<a target ='_blank' href='${myLeads[i]}'>
-  ${myLeads[i]}
+
+console.log(localStorage)
+if(leadsFromLocalStorage){
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
+}
+function render(Leads) {
+  let listItem = ""
+  for (let i = 0; i < Leads.length; i++) {
+    listItem += `<li id='li-el'>
+<a target ='_blank' href='${Leads[i]}'>
+  ${Leads[i]}
 </a>
 </li>`;
- }
- ulEl.innerHTML = listItem
+  }
+  ulEl.innerHTML = listItem
 }
-
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads)
+})
+console.log(leadsFromLocalStorage);
+inputBtn.addEventListener("click", function () {
+ 
+ myLeads.push(inputEl.value);
+ inputEl.value = "";
+ localStorage.setItem("myLeads",JSON.stringify(myLeads))
+ render(myLeads);
+})  
 
 
 
